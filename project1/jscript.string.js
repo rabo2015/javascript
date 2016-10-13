ae35202e4336e94c0cc495a0171acefcda57ec29
rabo2015 <helen@helendeMacBook-Air.local>
2016-10-13 23:59:29 +0800

    jisuanqi

--- project1/jscript.string.js
+++ project1/jscript.string.js
@@ -44,16 +44,87 @@ jscript.string.stripChars=function(inStr,inStripOrAllow,inCharList){
     }//end stripChars().
 
 
-    jscript.string.strContentValid=function(inString,inCharList,inFormExcept){
-        if(inString==""||inCharList==null||inFormExcept==null||
-            inString==""||inCharList==""){
-            return false;
-        }
-        inFormExcept=inFormExcept.toLowerCase();
-        var i;
-        if(inFormExcept=="for_list"){
-
-        }
+jscript.string.strContentValid=function(inString,inCharList,inFormExcept){
+    if(inString==""||inCharList==null||inFormExcept==null||
+        inString==""||inCharList==""){
+        return false;
     }
+    inFormExcept=inFormExcept.toLowerCase();
+    var i;
+    if(inFormExcept=="form_list"){
+        if(inCharList.indexOf(inString.cjarAt(i))==-1){
+            return false;
+        }               
+    }
+    return true;
+}//end strContentValid();
 
+
+
+jscript.string.replace=function(inSrc,inOld,inNew){
+    if(inSrc==null||inSrc==""||inOld==null||inOld==""||
+      inNew==null||inNew==""){
+        return "";
+    }
+    while(inSrc.indexOf(inOld)>-1){
+        inSrc=inSrc.replace(inOld,inNew);
+    }
+    return inSrc;
+}//end replace
+
+
+jscript.string.leftTrim=function(inStr){
+    if(inStr==null||inStr==""){
+        return null;
+    }
+    var j;
+    for(j=0;inStr.charAt(j)=="";j++){
+        return inStr.substring(j,inStr.length);
+    }
+}//end leftTrim.
+
+jscript.string.rightTrim=function(inStr){
+    if(inStr==null||inStr==""){
+        return null;
+    }
+    instr=this.leftTrim(inStr);
+    instr=this.rightTrim(inStr);
+    return inStr;
+}//end rightTrim.
+
+
+jscript.string.breakLine=function(inText,inSize){
+    
+    if(inText==null||inText==""||inSize<=0){
+        return inText;
+    }
+    if(inText.length<=inSize){
+        return inText;
+    }
+    var outArray=new Array();
+    var str =inText;
+    while(str.length>inSize){
+        var x=str.substring(0,inSize);
+        var y=x.lastIndexOf(" ");
+        var z=x.lastIndexOf("\n");
+        if(z!=-1){
+            y=z;
+        }
+        if(y==-1){
+            y=inSize;
+        }
+        outArray.push(str.substring(0,y));
+        str=str.substring(y);
+    }
+    outArray.push(str);
+    return outArray;
+    
+}//end break Line();
+    
+    
+    
+    
 }
+
+
+
